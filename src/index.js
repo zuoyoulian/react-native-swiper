06a2a694bed562cce5af2b81b45216cd3fc73572
zuojianjun <zuojianjun@tuofeng.cn>
2017-10-25 13:04:15 +0800

    添加滚动到顶部的方法

--- src/index.js
+++ src/index.js
@@ -414,6 +414,12 @@ export default class extends Component {
     }
   }
 
+  scrollToTop(animated) {
+    if (this.refs.scrollView) {
+       this.scrollBy(-this.state.index, animated)
+    }
+  }
+
   scrollViewPropOverrides = () => {
     const props = this.props
     let overrides = {}
